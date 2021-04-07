eab72cd1cb94e2d6890b3a0bf5b55578dee74bab
npex42 <npex42@gmail.com>
2021-04-07 17:52:51 +0100

    Added INSTRUCT_WORD Constant

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rust_vm::RustVM;
+
 
 mod rust_vm;
 
@@ -18,7 +18,7 @@ fn test_init() {
     let mut vm = rust_vm::RustVM::new();
     vm.clock();
     assert_eq!(2, vm.get_ip());
-    assert_eq!(rust_vm::INSTRUCTION_BYTES, RustVM::get_instruction_length())
+    assert_eq!(rust_vm::INSTRUCTION_BYTES, rust_vm::RustVM::get_instruction_length())
 }
 
 #[cfg(test)]
@@ -32,7 +32,13 @@ fn test_ip_change() {
 #[cfg(test)]
 #[test]
 fn test_instruction_length() { //Instruction length may be critical to several part in the future 
-    assert_eq!(rust_vm::INSTRUCTION_BYTES, RustVM::get_instruction_length())
+    assert_eq!(rust_vm::INSTRUCTION_BYTES, rust_vm::RustVM::get_instruction_length())
+}
+
+#[cfg(test)]
+#[test]
+fn test_instruction_word_count() { //Instruction length may be critical to several part in the future 
+    assert_eq!(rust_vm::INSTRUCTION_WORDS, rust_vm::RustVM::get_instruction_length() / 2)
 }
 
 
